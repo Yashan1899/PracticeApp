99fe7b545b3b74fdb6f4e39aa096d7f4f36342af
Yashan1899 <64845205+Yashan1899@users.noreply.github.com>
2020-11-12 12:15:49 +0200

    Добавлен условный оператор

--- PracticeFitnessApp/Program.cs
+++ PracticeFitnessApp/Program.cs
@@ -9,6 +9,10 @@ namespace PracticeFitnessApp
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello, Nick");
             int a = 4;
+            if (a == 4)
+            {
+                Console.WriteLine(a);
+            }
         }
     }
 }
